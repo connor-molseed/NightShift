674d48401768c08869ab1a6a957373b6ab30fda1
connor-molseed <connor.molseed.95@gmail.com>
2026-07-27 11:09:48 +0930

    Carried objects can now be dropped

--- Assets/Scripts/Interaction/CarryController.cs
+++ Assets/Scripts/Interaction/CarryController.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarryController : MonoBehaviour
 {
     //Config Parameters
     [SerializeField] protected Transform _itemAttachPoint;
+    [SerializeField] protected InputActionReference _dropButton;
+    [SerializeField] protected float _dropDistance;
     
     //State Variables
     protected Carryable _heldItem;
-    
+
     //Cached References
-    
+
     //Properties
-    
+
     //Events
-    
+    void OnEnable()
+    {
+        Debug.Log("Doing an enable");
+        _dropButton.action.Enable();
+        _dropButton.action.performed += HandleDropInput;
+    }
 
+    void OnDisable()
+    {
+        Debug.Log("Doing a disable");
+        _dropButton.action.performed -= HandleDropInput;
+    }
 
-    public bool PickupObject(Carryable obj)
+    public void HandleDropInput(InputAction.CallbackContext context)
+    {  
+        if (_heldItem == null) return;
+
+        TryDropObject(GetDropLocation());
+    }
+
+    public bool TryPickupObject(Carryable obj)
     {
         if (obj == null) return false;
 
@@ -31,23 +51,30 @@ public class CarryController : MonoBehaviour
         _heldItem.transform.localPosition = Vector3.zero;
         _heldItem.transform.localRotation = _itemAttachPoint.localRotation;
         _heldItem.gameObject.layer = LayerMask.NameToLayer("Player");
+        _heldItem.EnablePhysics(false);
 
         Debug.Log($"Now carrying {_heldItem.name}");
         
         return true;
     }
 
-    public bool DropObject(Vector3 location)
+    public bool TryDropObject(Vector3 location)
     {
         if (_heldItem == null) return false;
 
         _heldItem.transform.SetParent(null);
         _heldItem.transform.position = location;
         _heldItem.gameObject.layer = _heldItem.OriginalLayer;
+        _heldItem.EnablePhysics(true);
 
         Debug.Log($"{_heldItem.name} has been put down again");
         _heldItem = null;
 
         return true;
+    }
+
+    protected Vector3 GetDropLocation()
+    {
+        return _itemAttachPoint.transform.position;
     }
 }
